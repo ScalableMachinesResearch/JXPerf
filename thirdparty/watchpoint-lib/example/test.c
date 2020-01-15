cb68f861e5d662cded9be190b04604772c1e84e3
Pengfei Su <psu@email.wm.edu>
2020-01-15 17:13:57 -0500

    a test case on the watchpoint lib

--- thirdparty/watchpoint-lib/example/test.c
+++ thirdparty/watchpoint-lib/example/test.c
@@ -1,13 +1,11 @@
 #include<stdio.h>
 #include"watchpoint.h"
 
-volatile int a = 0;
-int b=11;
-
+volatile int a = 1;
 
 WP_TriggerAction_t OnTrap(WP_TriggerInfo_t *info){
 
-    printf("One trap, secret %d, pc %lx, %s\n", *((int *)info->data), info->pc, info->pcPrecise==1?"Precise":"Imprecise");
+    printf("One trap, value %d, address %p\n", *((int *)info->va), info->va);
     return WP_DISABLE;
 }
 
@@ -18,7 +16,7 @@ int main(int argc, char **argv){
 
     WP_ThreadInit(OnTrap);
     printf("The address of a is %p\n", &a);
-    WP_Subscribe(&a, 1, WP_RW, &b);
+    WP_Subscribe((void *)(&a), sizeof(int), WP_RW, sizeof(int), NULL, 0, true);
     a += 1;
 
     WP_ThreadTerminate(0);
