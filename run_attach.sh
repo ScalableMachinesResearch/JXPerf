f66bf7d3d9b6e7431404dc4d6b9e89ccd17a31c9
bolunli11 <bli11@email.wm.edu>
2020-06-07 14:54:16 -0400

    receive pid from CLI

--- run_attach.sh
+++ run_attach.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 ATTACH=$JXPerf_HOME/attach/attach
-DURATION="$1"
+PID="$1"
+DURATION="$2"
 pid=100041
 
-"$ATTACH" "$pid" load instrument false $JXPerf_HOME/thirdparty/allocation-instrumenter/target/java-allocation-instrumenter-HEAD-SNAPSHOT.jar
-"$ATTACH" "$pid" load $JXPerf_HOME/build/libagent.so true NUMA::MEM_LOAD_UOPS_RETIRED:L1_MISS:precise=2@10000s
+"$ATTACH" "$PID" load instrument false $JXPerf_HOME/thirdparty/allocation-instrumenter/target/java-allocation-instrumenter-HEAD-SNAPSHOT.jar
+"$ATTACH" "$PID" load $JXPerf_HOME/build/libagent.so true NUMA::MEM_LOAD_UOPS_RETIRED:L1_MISS:precise=2@10000s
 while (( DURATION-- > 0 )) 
 do
     sleep 1
 done
-"$ATTACH" "$pid" load $JXPerf_HOME/build/libstop.so true NUMA::MEM_LOAD_UOPS_RETIRED:L1_MISS:precise=2@10000p
+"$ATTACH" "$PID" load $JXPerf_HOME/build/libstop.so true NUMA::MEM_LOAD_UOPS_RETIRED:L1_MISS:precise=2@10000p
