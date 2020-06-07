be87d269aba439dbeac8d82da39404caa6c9002b
bolunli11 <bli11@email.wm.edu>
2020-06-07 15:02:44 -0400

    minor

--- run_attach.sh
+++ run_attach.sh
@@ -3,7 +3,6 @@
 ATTACH=$JXPerf_HOME/attach/attach
 PID="$1"
 DURATION="$2"
-pid=100041
 
 "$ATTACH" "$PID" load instrument false $JXPerf_HOME/thirdparty/allocation-instrumenter/target/java-allocation-instrumenter-HEAD-SNAPSHOT.jar
 "$ATTACH" "$PID" load $JXPerf_HOME/build/libagent.so true NUMA::MEM_LOAD_UOPS_RETIRED:L1_MISS:precise=2@10000s
