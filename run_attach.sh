a6537da1b9cb754115f70ee9fb15a85a8c383bea
bolunli11 <bli11@email.wm.edu>
2020-06-17 13:42:24 -0400

    support changing mode in run_attach script

--- run_attach.sh
+++ run_attach.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 ATTACH=$JXPerf_HOME/attach/attach
+MODE=DataCentric::MEM_LOAD_UOPS_RETIRED:L1_MISS:precise=2@1000000
 PID="$1"
 DURATION="$2"
 
 "$ATTACH" "$PID" load instrument false $JXPerf_HOME/thirdparty/allocation-instrumenter/target/java-allocation-instrumenter-HEAD-SNAPSHOT.jar
-"$ATTACH" "$PID" load $JXPerf_HOME/build/libagent.so true NUMA::MEM_LOAD_UOPS_RETIRED:L1_MISS:precise=2@10000s
+"$ATTACH" "$PID" load $JXPerf_HOME/build/libagent.so true "$MODE"s
 while (( DURATION-- > 0 )) 
 do
     sleep 1
 done
-"$ATTACH" "$PID" load $JXPerf_HOME/build/libstop.so true NUMA::MEM_LOAD_UOPS_RETIRED:L1_MISS:precise=2@10000p
+"$ATTACH" "$PID" load $JXPerf_HOME/build/libstop.so true "$MODE"p
